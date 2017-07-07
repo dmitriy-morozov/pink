1d8b6f2cc4b2057831bae8d229d6bc5f7e439592
morozov <morozov@quartsoft.com>
2017-07-07 17:37:40 +0300

    Рефакторинг и замена слайдеров

--- src/js/filter-icon.js
+++ src/js/filter-icon.js
@@ -1,31 +1,24 @@
+$(function () {
+    if ($(window).width() < 699) {
+        $(".filter--contrast").click(function (event) {
+            $(".filter--contrast").toggleClass("active", 500);
+            $(".filter--fill, .filter--crop").removeClass("active", 500);
+            $(".slider-3").toggle();
+            $(".slider-1, .slider-2").hide();
+        });
 
-$(function() { 
-  if ($(window).width() < 699) {
-    $(".filter--contrast").click(function(event) {
-      $(".filter--contrast").toggleClass("active",500);
-      $(".filter--fill").removeClass("active",500);
-      $(".filter--crop").removeClass("active",500);
-      $(".slider-3").toggle();
-      $(".slider-2").hide();
-      $(".slider-1").hide();
-    });
+        $(".filter--fill").click(function (event) {
+            $(".filter--fill").toggleClass("active", 500);
+            $(".filter--contrast, .filter--crop").removeClass("active", 500);
+            $(".slider-2").toggle();
+            $(".slider-3, .slider-1").hide();
+        });
 
-    $(".filter--fill").click(function(event) {
-      $(".filter--fill").toggleClass("active",500);
-      $(".filter--contrast").removeClass("active",500);
-      $(".filter--crop").removeClass("active",500);
-      $(".slider-2").toggle();
-      $(".slider-3").hide();
-      $(".slider-1").hide();
-    });
-
-    $(".filter--crop").click(function(event) {
-      $(".filter--crop").toggleClass("active",500);
-      $(".filter--fill").removeClass("active",500);
-      $(".filter--contrast").removeClass("active",500);
-      $(".slider-1").toggle();
-      $(".slider-2").hide();
-      $(".slider-3").hide();
-    })
-  }
+        $(".filter--crop").click(function (event) {
+            $(".filter--crop").toggleClass("active", 500);
+            $(".filter--fill, .filter--contrast").removeClass("active", 500);
+            $(".slider-1").toggle();
+            $(".slider-2, .slider-3").hide();
+        })
+    }
 })
---
+++ src/js/slider.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    $(".price__item, .reviews__item, .slider2, .slider").removeClass("slider--no-js");
+
+    $('.slider').slick({
+        "speed": 300,
+        "autoplay": true,
+        "infinite": true,
+        "slidesToShow": 1,
+        "slidesToScroll": 1,
+        "arrows": false,
+        "dots": true,
+        "dotsClass": "bullets"
+    });
+
+    $('.reviews .prev').on('click', function () {
+        $('.slider').slick("slickPrev");
+    });
+
+    $('.reviews .next').on('click', function () {
+        $('.slider').slick("slickNext");
+    });
+});
+
+
+$(document).ready(slider2);
+$(window).on('resize', slider2);
+
+function slider2() {
+    var slickOpts = {
+        "speed": 300,
+        "autoplay": true,
+        "infinite": true,
+        "slidesToShow": 1,
+        "slidesToScroll": 1,
+        "arrows": false
+    };
+
+    $('.slider2').not('.slick-initialized').slick(slickOpts);
+
+    if ($(window).width() > 699) {
+        $('.slider2')[0].slick.unslick();
+    } else {
+        $('.slider2').slick(slickOpts);
+    }
+
+    $('.price .prev').on('click', function () {
+        $('.slider2').slick("slickPrev");
+    });
+
+    $('.price .next').on('click', function () {
+        $('.slider2').slick("slickNext");
+    });
+}
