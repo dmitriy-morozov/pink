e7d81f241a53a9e1ea5b767dcba1f9944b7788f3
morozov <morozov@quartsoft.com>
2017-07-07 15:38:14 +0300

    Test

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,5 @@
-$(document).ready(function(){$(".main-nav__header").removeClass("main-nav__header--no-js"),$(".main-nav__list").removeClass("main-nav__list--no-js"),$(".main-nav__button").click(function(n){$(".main-nav__list").toggleClass("main-nav__list--closed",5e3),$(".main-nav__button").toggleClass("main-nav__button--close"),$(".main-nav__header").toggleClass("main-nav__header--open")})});
+$(document).ready(function () {
+    $(".main-nav__header").removeClass("main-nav__header--no-js"), $(".main-nav__list").removeClass("main-nav__list--no-js"), $(".main-nav__button").click(function (n) {
+        $(".main-nav__list").toggleClass("main-nav__list--closed", 5e3), $(".main-nav__button").toggleClass("main-nav__button--close"), $(".main-nav__header").toggleClass("main-nav__header--open")
+    })
+});
